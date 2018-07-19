f34257ae60214d10ba42f4c849eb387d1ff1c153
Laurence <laurencemullen99@gmail.com>
2018-07-19 00:20:03 -0400

    Video responsiveness @media screen width>height

--- responsive_fullscreen.js
+++ responsive_fullscreen.js
@@ -1,20 +1,22 @@
-sqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fill<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <!--/*//<!--//sqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fillsqs-video-background content-fill-->*/-->$(document).ready(function(){
+$(document).ready(function(){
     // - 1.78 is the aspect ratio of the video
     // - This will work if your video is 1920 x 1080
     // - To find this value divide the video's native width by the height eg 1920/1080 = 1.78
     var aspectRatio = 1.77777777;
-    var video = $('.videocontainer iframe');
+    var video = $('.videocontainer');
     var videoHeight = video.outerHeight();
     var newWidth = videoHeight*aspectRatio;
-    var halfNewWidth = newWidth/2;
 	
 	//height adjust
-	var container = $('.videocontainer');
-	var videoWidth = container.outerWidth();
-	var newHeightPer = videoWidth/newWidth;
+	var videoWidth = video.outerWidth();
+	var newHeightPer = (videoWidth/newWidth)*100;
+	
+	//width adjust
+	var newWidthPer = (videoHeight/(videoWidth*aspectRatio))*100;
 
-    video.css({"width":newWidth+"px","left":"50%","margin-left":"-"+halfNewWidth+"px"});
-
+    $('#videoiframe').css({"height":newHeightPer+"%"});
+	//, "width":newWidthPer+"%"
+	console.log(newWidthPer);
 });
 
 $(window).on('resize', function() {
@@ -24,12 +26,11 @@ $(window).on('resize', function() {
     var video = $('.videocontainer iframe');
     var videoHeight = video.outerHeight();
     var newWidth = videoHeight*aspectRatio;
-    var halfNewWidth = newWidth/2;
 	
 	//height adjust
 	var container = $('.videocontainer');
 	var videoWidth = container.outerWidth();
-	var newHeightPer = videoWidth/newWidth;
+	var newHeightPer = (videoWidth/newWidth)*100;
 
-    video.css({"width":newWidth+"px","left":"50%","margin-left":"-"+halfNewWidth+"px"});
+    video.css({"height":newHeightPer+"%"});
 });
